cf7d9713f273920ad84d06b4952ea7a5e9958fc3
toast <cpastoast@protonmail.com>
2022-05-09 06:49:06 +0200

    code improvement

--- utils/randomize/randomize.go
+++ utils/randomize/randomize.go
@@ -15,7 +15,6 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	// shuffle the lines
 	for i := len(lines) - 1; i > 0; i-- {
 		j := rand.Intn(i + 1)
 		lines[i], lines[j] = lines[j], lines[i]
