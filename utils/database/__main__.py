e351123374974fc830f61c35d8006351b643b632
toast <cpastoast@protonmail.com>
2022-05-09 07:24:20 +0200

    update

--- utils/database/__main__.py
+++ utils/database/__main__.py
@@ -6,12 +6,22 @@ from pymongo.mongo_client import MongoClient
 from .models import Output
 
 MONGO_URL = ""
+MAX_SIZE = 20
 
 
 if __name__ == "__main__":
 
     client: MongoClient = MongoClient(MONGO_URL)
 
+    results = []
+
     for line in sys.stdin:
-        output = Output(**json.loads(line))
-        client.db.output.insert_one(output.dict())
+
+        try:
+            results.append(Output(**json.loads(line)).dict())
+        except Exception:
+            continue
+
+        if len(results) == MAX_SIZE:
+            client.db.servers.insert_many(results)
+            results = []
