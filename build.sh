7b672469b84622f8f8486b9c08f6932223c7c440
toast <cpastoast@protonmail.com>
2022-05-13 13:34:30 +0200

    a

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ export GB_OUTPUT="out/"
 rm -rf $GB_OUTPUT
 mkdir -p $GB_OUTPUT
 
-go build -o $GB_OUTPUT/minecraft check/minecraft.go
-go build -o $GB_OUTPUT/minecraft-parse check/parse/parse.go
-go build -o $GB_OUTPUT/generate generate/generate.go
-go build -o $GB_OUTPUT/randomize randomize/randomize.go
-go build -o $GB_OUTPUT/rcon-brute rcon/brute.go
+go build -o $GB_OUTPUT/minecraft minecraft.go
+go build -o $GB_OUTPUT/minecraft-parse minecraft-parse.go
+go build -o $GB_OUTPUT/generate generate.go
+go build -o $GB_OUTPUT/randomize randomize.go
+go build -o $GB_OUTPUT/rcon-brute brute.go
 
-curl -O https://raw.githubusercontent.com/ignis-sec/Pwdb-Public/master/wordlists/ignis-10K.txt
-curl -O https://raw.githubusercontent.com/ignis-sec/Pwdb-Public/master/wordlists/ignis-100K.txt
+curl -o out/10k.txt https://raw.githubusercontent.com/ignis-sec/Pwdb-Public/master/wordlists/ignis-10K.txt
+curl -o out/100k.txt https://raw.githubusercontent.com/ignis-sec/Pwdb-Public/master/wordlists/ignis-100K.txt
